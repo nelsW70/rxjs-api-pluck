e43c101b192709af4f95bc5fdbad264be151e0d3
Nelson Foltz <nelsfoltz@gmail.com>
2020-05-10 10:24:35 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface StarWars {
+  count: number;
+  next: string;
+  previous: any;
+  results: any[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sunday-test';
+export class AppComponent implements OnInit {
+  characters: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<StarWars>('https://swapi.dev/api/people/')
+      .pipe(pluck('results'))
+      .subscribe((response) => {
+        this.characters = response;
+        console.log(response);
+      });
+  }
 }
